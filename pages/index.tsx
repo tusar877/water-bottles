308900d88413fa15721a1dd4b7c75bcf2a4b22b3
tusar877 <130102846+tusar877@users.noreply.github.com>
2023-07-01 17:16:57 +0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   useEffect(() => {
-    window.location.href = 'https://amzn.to/42aFW0T';
+    window.location.href = 'https://amzn.to/448bNB8';
   }, []);
 
   return null;
