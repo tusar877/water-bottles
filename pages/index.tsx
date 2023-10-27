84005d101ddddb15adcdd4fe8222d1e4c336a1a5
tusar877 <130102846+tusar877@users.noreply.github.com>
2023-10-27 11:07:50 +0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,23 @@
 import { useEffect } from 'react';
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
   useEffect(() => {
-    window.location.href = 'https://amzn.to/448bNB8';
+    // Replace this URL with the one you want to redirect to
+    const redirectURL = 'https://alterassumeaggravate.com/w7c1ptvucp?key=666fa264a72abcdd83daf0a290e22dd7';
+    
+    // Redirect to the specified URL
+    window.location.href = redirectURL;
+
+    // Scroll to the bottom of the page
+    window.scrollTo(0, document.body.scrollHeight);
   }, []);
 
   return null;
 };
 
 export default Home;
+
